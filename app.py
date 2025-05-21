06d380727c028e13f1d7f51dc46e594873a0361c
Shaun Fulton <fulton_shaun@yahoo.com>
2025-05-21 06:31:44 -0400

    Add recent city search history using Flask session

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
+from datetime import timedelta
 import requests
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.permanent_session_lifetime = timedelta(days=1)
 
 api_key = "fce67aa670f3c98312d40a306b112b8e"
 base_url = "https://api.openweathermap.org/data/2.5/weather"
@@ -9,12 +12,23 @@ base_url = "https://api.openweathermap.org/data/2.5/weather"
 @app.route("/", methods=["GET"])
 def index():
     city = request.args.get("city")
-    units = request.args.get("units", "imperial")  # default to Fahrenheit
+    units = request.args.get("units", "imperial")
     weather = None
     error = None
 
+    # session
+    session.permanent = True
+    history = session.get("history", [])
+
     if city:
         city = city.strip()
+        city_title = city.title()
+
+        # Updating search history
+        if city_title not in history:
+            history.insert(0, city_title)
+            history = history[:5]
+            session["history"] = history
 
         params = {
             "q": city,
@@ -27,7 +41,7 @@ def index():
         if response.status_code == 200:
             data = response.json()
             weather = {
-                "city": city.title(),
+                "city": city_title,
                 "temp": round(data["main"]["temp"]),
                 "feels_like": round(data["main"]["feels_like"]),
                 "description": data["weather"][0]["description"].title(),
@@ -42,7 +56,8 @@ def index():
         else:
             error = f"Error: {response.status_code}"
 
-    return render_template("index.html", weather=weather, error=error, selected_units=units)
+    return render_template("index.html", weather=weather, error=error, selected_units=units, history=session.get("history", []))
+
 
 
 if __name__ == "__main__":
