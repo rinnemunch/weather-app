0bc181fe125247cc99c5f3c2c38fed53cf73f8cc
Shaun Fulton <fulton_shaun@yahoo.com>
2025-05-21 05:44:32 -0400

    Add basic HTML form to enter city in browser

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import requests
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ def index():
   city = request.args.get("city")
 
   if not city:
-    return "Please provide a city. Example: /?city=London"
+    return render_template("index.html")
 
   city = city.strip()
 
